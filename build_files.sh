a0a81660f28a79387a060ce9044b620a47e1bee4
Sk <techjk021@gmail.com>
2024-07-02 20:31:32 +0530

    Updated files

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo "BUILD START"
 python3.12 -m pip install -r requirements.txt
-python3.12 manage.py makemigrations --no-input
-python3.12 manage.py migrate --no-input
-python3.12 manage.py collectstatic --no-input --clear 
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
+python3.12 manage.py collectstatic --noinput --clear 
 echo "BUILD END"
