d38d166d600566e2bcec009b45a2a6064aaa2f82
Sk <techjk021@gmail.com>
2024-07-02 16:46:02 +0530

    Updated files

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo "BUILD START"
-apt-get update && apt-get install -y sqlite3 libsqlite3-dev
+apt-get update && apt-get install -y sqlite3 libsqlite3-dev sqlite-devel
 apt install libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev
 apt install libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev libffi-dev uuid-dev
 python3.12 -m pip install -r requirements.txt
