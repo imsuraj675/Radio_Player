268778f4afcb56512236eb7a8c5422833ab631d4
Sk <techjk021@gmail.com>
2024-07-02 17:52:33 +0530

    Updated files

--- music_room/settings.py
+++ music_room/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 # from dotenv import load_dotenv
 import dj_database_url
 from decouple import config
-# from os import environ, path
+from os import path
 
 # load_dotenv()
 
@@ -127,8 +127,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = path.join(BASE_DIR, 'staticfiles')
 
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
