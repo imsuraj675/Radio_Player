d3e4288f432797e9f5a59979562f2dfcf969e60b
Sk <techjk021@gmail.com>
2024-07-02 17:46:14 +0530

    Updated files

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,7 @@
 echo "BUILD START"
+python3.12 pip install virtualenv
+virtualenv env
+.\env\Scripts\activate
 python3.12 -m pip install -r requirements.txt
 python3.12 manage.py makemigrations --no-input
 python3.12 manage.py migrate --no-input
--- music_room/settings.py
+++ music_room/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    # "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
