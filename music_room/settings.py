f54f25d4a69fe7c3bce0d9dee2694c97e4298030
Suraj Kashyap <120774752+imsuraj675@users.noreply.github.com>
2024-07-02 21:15:59 +0530

    Update settings.py

--- music_room/settings.py
+++ music_room/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-dpu3vvqcpg613ite!un!6xqwri&^jfptwh$bh9@vjwo75g&wiq"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".vercel.app", ".now.sh", "127.0.0.1"]
 
